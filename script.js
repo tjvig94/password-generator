43b07bcef6e0950ae1d2f77a155241e4e6aaa53c
Tim Vigneau <tjvig94@gmail.com>
2020-12-21 17:45:41 -0500

    Added js code for password generation

--- script.js
+++ script.js
@@ -2,15 +2,35 @@ function updateTextInput(val) {
     document.getElementById('textInput').value=val; 
   }
 
+// function generatePassword() {
+//   let charset = 
+//       length = 8,
+//       password = "";
+//       for (var i = 0, n = charset.length; i < length; ++i) {
+//         password += charset.charAt(Math.floor(Math.random() * n));
+        
+//       }
+//       return password;
+// }
 
-  document.getElementById("generate").addEventListener("click", () => {
+function generatePassword1() {
+  let length = 8,
+      password = "";
+      for (var i = 0; i < length; i++) {
+        if (document.getElementById("btncheck1").checked == true) {
+          password += String.fromCharCode(Math.floor(Math.random()*26)+97);
+        } else if (document.getElementById("btncheck2").checked == true) {
+          password += String.fromCharCode(Math.floor(Math.random()*26)+65);
+        } else if (document.getElementById("btncheck2").checked == true) {
+          password += String.fromCharCode(Math.floor(Math.random()*10)+48);
+        } else if (document.getElementById("btncheck2").checked == true) {
+          password += String.fromCharCode(Math.floor(Math.random()*15)+33);
+        }
+        
+        else {
+          prompt("You need to check a box.");
+        }
+      }
 
-          let length = document.getElementyById("textInput").value,
-              charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-              password = "";
-          for (var i = 0, n = charset.length; i < length; i++) {
-                password += charset.charAt(Math.floor(Math.random() * n));
-    
-            }
-            return password);
+      return password
 }
