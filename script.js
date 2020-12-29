e08ca1a5637bbc115392b0032f576f3b7bd7d77a
Tim Vigneau <tjvig94@gmail.com>
2020-12-28 21:34:37 -0500

    Updated code block for character selection

--- script.js
+++ script.js
@@ -2,28 +2,31 @@ function updateTextInput(val) {
     document.getElementById('textInput').value=val; 
   }
 
-function generatePassword() {
-  let length = document.getElementById('customRange1').value,
-      password = "";
-      for (var i = 0; i < length; i++) {
-        if (document.getElementById("btncheck1").checked == true) {
-          password += String.fromCharCode(Math.floor(Math.random()*26)+97);
-        }  else {}
-        if (document.getElementById("btncheck2").checked == true) {
-          password += String.fromCharCode(Math.floor(Math.random()*26)+65);
-        } else{} 
-        if (document.getElementById("btncheck3").checked == true) {
-          password += String.fromCharCode(Math.floor(Math.random()*10)+48);  
-        }  else {}
-        if (document.getElementById("btncheck4").checked == true) {
-          password += String.fromCharCode(Math.floor(Math.random()*15)+33);
-        }
-        
-        else {}
-      }
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+var special = "~!@#$%^&*-_=+/?><";
 
-      document.getElementById("generate").placeholder = password;
+var characters = "";
+(btncheck1.checked) ? characters += lower : "";
+(btncheck2.checked) ? characters += upper : "";
+(btncheck3.checked) ? characters += num : "";
+(btncheck4.checked) ? characters += special : "";
+
+
+
+function generatePassword() {
+  (btncheck1.checked) ? characters += lower : "";
+  (btncheck2.checked) ? characters += upper : "";
+  (btncheck3.checked) ? characters += num : "";
+  (btncheck4.checked) ? characters += special : "";
+  let passLength = document.getElementById('customRange1').value,
+      password = "";
+  for (var i = 0; i < passLength; i++) {
+      password += characters.charAt(Math.floor(Math.random()*characters.length));
+    }
+
+  document.getElementById("generate").placeholder = password;
 }
 
 passwordGen.addEventListener("click", generatePassword);
-
