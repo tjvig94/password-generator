c0716776b03f605fa0423b3b9467b9685aa344f1
Tim Vigneau <tjvig94@gmail.com>
2020-12-28 20:23:44 -0500

    Added functionality to Generate password button

--- script.js
+++ script.js
@@ -2,35 +2,28 @@ function updateTextInput(val) {
     document.getElementById('textInput').value=val; 
   }
 
-// function generatePassword() {
-//   let charset = 
-//       length = 8,
-//       password = "";
-//       for (var i = 0, n = charset.length; i < length; ++i) {
-//         password += charset.charAt(Math.floor(Math.random() * n));
-        
-//       }
-//       return password;
-// }
-
-function generatePassword1() {
-  let length = 8,
+function generatePassword() {
+  let length = document.getElementById('customRange1').value,
       password = "";
       for (var i = 0; i < length; i++) {
         if (document.getElementById("btncheck1").checked == true) {
           password += String.fromCharCode(Math.floor(Math.random()*26)+97);
-        } else if (document.getElementById("btncheck2").checked == true) {
+        }  else {}
+        if (document.getElementById("btncheck2").checked == true) {
           password += String.fromCharCode(Math.floor(Math.random()*26)+65);
-        } else if (document.getElementById("btncheck2").checked == true) {
-          password += String.fromCharCode(Math.floor(Math.random()*10)+48);
-        } else if (document.getElementById("btncheck2").checked == true) {
+        } else{} 
+        if (document.getElementById("btncheck3").checked == true) {
+          password += String.fromCharCode(Math.floor(Math.random()*10)+48);  
+        }  else {}
+        if (document.getElementById("btncheck4").checked == true) {
           password += String.fromCharCode(Math.floor(Math.random()*15)+33);
         }
         
-        else {
-          prompt("You need to check a box.");
-        }
+        else {}
       }
 
-      return password
+      document.getElementById("generate").placeholder = password;
 }
+
+passwordGen.addEventListener("click", generatePassword);
+
