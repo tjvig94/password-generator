f364738cf78f86489cd420a9fe5e35755446667c
Tim Vigneau <tjvig94@gmail.com>
2021-01-04 15:42:16 -0500

    Updated checkbox layout, updated button id's

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var oopsModal = new bootstrap.Modal(document.getElementById("oopsModal"), {
+const oopsModal = new bootstrap.Modal(document.getElementById("oopsModal"), {
   keyboard: false
 })
 
@@ -6,24 +6,24 @@ function updateTextInput(val) {
     document.getElementById('textInput').innerHTML=val; 
 }
 
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var num = "0123456789";
-var special = "~!@#$%^&*-_=+/?><";
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const num = "0123456789";
+const special = "~!@#$%^&*-_=+/?><";
 
 function generatePassword() {
   let characters = "";
-  (btncheck1.checked) ? characters += lower : "";
-  (btncheck2.checked) ? characters += upper : "";
-  (btncheck3.checked) ? characters += num : "";
-  (btncheck4.checked) ? characters += special : "";
-  let passLength = document.getElementById('customRange1').value;
+  (lowercase.checked) ? characters += lower : "";
+  (uppercase.checked) ? characters += upper : "";
+  (numbers.checked) ? characters += num : "";
+  (symbols.checked) ? characters += special : "";
+  let passLength = document.getElementById("charNumber").value;
   let password = "";
-  for (var i = 0; i < passLength; i++) {
+  for (let i = 0; i < passLength; i++) {
       password += characters.charAt(Math.floor(Math.random()*characters.length));
     }
 
-  (btncheck1.checked == false && btncheck2.checked == false && btncheck3.checked == false && btncheck4.checked == false) ? oopsModal.show() : document.getElementById("passBox").value = password;
+  (lowercase.checked == false && uppercase.checked == false && numbers.checked == false && symbols.checked == false) ? oopsModal.show() : document.getElementById("passBox").value = password;
 }
 
 passwordGen.addEventListener("click", generatePassword);
